14f5a98164941bcc05b7d029fa79aadae22d105e
Alejandro Gomez <agomez314@protonmail.com>
2022-02-06 09:18:52 -0500

    fill exercise

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,64 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(LOOP)
+  @KBD
+  D=M
+
+  @ON
+  D;JGT
+
+  @OFF
+  0;JMP
+
+(ON)
+  @R0
+  M=-1
+
+  @DRAW
+  0;JMP
+
+(OFF)
+  @R0
+  M=0
+
+  @DRAW
+  0;JMP
+
+(DRAW)
+  // Draw every single bit in screen memory address space
+  @8191 // total number of screen address bits
+  D=A
+  @R1
+  M=D
+
+  (NEXT)
+    // calculate the position
+    @R1
+    D=M
+    @pos
+    M=D
+    @SCREEN
+    D=A
+    @pos
+    M=M+D //Add base screen address to curr position
+
+    // Draw the value at pos' current address
+    @R0
+    D=M
+    @pos
+    A=M
+    M=D
+
+    // Decrement the counter
+    @R1
+    D=M-1
+    M=D
+
+    @NEXT
+    D;JGE
+
+  // Loop back around
+  @LOOP
+  0;JMP
+
