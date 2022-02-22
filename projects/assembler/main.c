26456202c898a7370509a1a8e98321c74da73a1a
Alejandro Gomez <agomez314@protonmail.com>
2022-02-22 05:36:59 -0500

    assembler progress

--- projects/assembler/main.c
+++ projects/assembler/main.c
@@ -20,57 +20,63 @@
 #include <string.h>
 #include <stdlib.h>
 
-// Key-val struct of a symbol with its corresponding address. Used for fixed table.
+struct keytab;
+struct List;
+int getword(char *, int);
+void initList(struct List *, int);
+void insert(struct List *, char *, int);
+void freeList(struct List *);
+void runFile(char *);
+void populate_symbol_table(struct List *);
+
+// Key-val struct of a symbol with its corresponding address.
 struct keytab {
   char *symbol;
   int address;
-}; 
-
-struct node {
-  char *symbol;
-  int address;
-  struct node *left;
-  struct node *right;
 };
 
-int getword(char *, int);
-
-// must be alphabetically ordered in order to perform bsearch
-struct keytab predefinedSymbols[] = {
-  { "ARG", 2 },
-  { "KBD", 24576 },
-  { "LCL", 1 },
-  { "R0", 0 }, 
-  { "R1", 1 },
-  { "R2", 2 },
-  { "R3", 3 },
-  { "R4", 4 },
-  { "R5", 5 },
-  { "R6", 6 },
-  { "R7", 7 },
-  { "R8", 8 },
-  { "R9", 9 },
-  { "R10", 10 },
-  { "R11", 11 },
-  { "R12", 12 },
-  { "R13", 13 },
-  { "R14", 14 },
-  { "R15", 15 },
-  { "SCREEN", 16384 },
-  { "SP", 0 },
-  { "THAT", 4 },
-  { "THIS", 3 }
+struct List {
+  struct keytab* array;
+  int used;
+  int size;
 };
+
+void initList(struct List *list, int initialSize) {
+  (*list).array = malloc(initialSize*sizeof(struct keytab));
+  (*list).used = 0;
+  (*list).size = initialSize;
+}
+
+void insert(struct List *list, char *symbol, int address) {
+  if ((*list).used == (*list).size) {
+    (*list).size *= 2;
+    (*list).array = realloc((*list).array, (*list).size*sizeof(struct keytab));
+    if (!list->array) {
+      free(list->array);
+      printf("Ran out of memory.\n");
+      exit(1);
+    }
+  }
+  (*list).array[(*list).used].address = address;
+  (*list).array[(*list).used].symbol = symbol;
+  (*list).used += 1;
+}
+
+void freeList(struct List *list) {
+  free((*list).array);
+  (*list).array = NULL;
+  (*list).used = (*list).size = 0;
+}
 
 // Stores destination. Values must translated to binary in instruction
 struct keytab destination[] = {
   { "A", 4  },   // A register
-  { "AD", 6 },  // A register and D register
-  { "AM", 5 },  // A register and RAM[A]
-  { "AMD", 7}, // A register, RAM[A] and D register
+  { "AD", 6 },   // A register and D register
+  { "AM", 5 },   // A register and RAM[A]
+  { "AMD", 7},   // A register, RAM[A] and D register
   { "D", 2  },   // D register
   { "M", 1  },   // RAM[A]
-  { "MD", 3 } // RAM[A] and D register
+  { "MD", 3 }    // RAM[A] and D register
 };
 
 struct keytab jump[] = {
@@ -123,109 +129,120 @@ struct keytab comp1[] = {
 //
 //}
 
-// a binary search method to find the value in a keytab given a word key
-int get(char *symbol, struct keytab keytab[], int n)
-{
-   int cond;
-   int low, high, mid;
+#define COMMENT //
+#define C_SEPERATOR ;
+#define C_EQUALS =
+#define PARENS (
+
+void extract_labels();
+
+void populate_symbol_table(struct List *symbol_table) {
+  initList(&(*symbol_table), 16);
+  insert(&(*symbol_table), "R0", 0);
+  insert(&(*symbol_table), "R1", 1); 
+  insert(&(*symbol_table), "R2", 2); 
+  insert(&(*symbol_table), "R3", 3); 
+  insert(&(*symbol_table), "R4", 4); 
+  insert(&(*symbol_table), "R5", 5); 
+  insert(&(*symbol_table), "R6", 6); 
+  insert(&(*symbol_table), "R7", 7); 
+  insert(&(*symbol_table), "R8", 8); 
+  insert(&(*symbol_table), "R9", 9); 
+  insert(&(*symbol_table), "R10", 10); 
+  insert(&(*symbol_table), "R11", 11); 
+  insert(&(*symbol_table), "R12", 12); 
+  insert(&(*symbol_table), "R13", 13); 
+  insert(&(*symbol_table), "R14", 14); 
+  insert(&(*symbol_table), "R15", 15); 
+  insert(&(*symbol_table), "SCREEN", 16384); 
+  insert(&(*symbol_table), "KBD", 24576); 
+  insert(&(*symbol_table), "SP", 0); 
+  insert(&(*symbol_table), "LCL", 1); 
+  insert(&(*symbol_table), "ARG", 2); 
+  insert(&(*symbol_table), "THIS", 3); 
+  insert(&(*symbol_table), "THAT", 4); 
+}
+
+int isWhitespace(char c) {
+  return c == ' ';
+}
+
+int isBlankLine(char c, FILE *fp) {
+  return c == '\n' && c == fgetc(fp);
+}
+
+int isComment(char c, FILE *fp) {
+  return c == '/' && c == fgetc(fp);
+}
+
+char *first_pass(FILE *fp) {
+  int chunk = 4096;
+  int counter = 0;
+  char *buffer = malloc(chunk);
+  char *test;
+  int c;
+  
+  if (!buffer) exit(1);
+
+  while ((c = fgetc(fp)) && (c != EOF)) {
+    //if (isWhitespace(c) || isBlankLine(c, fp) || isComment(c, fp))
+    //  continue;
+    
+    buffer[counter++] = c;
    
-   low = 0;
-   high = n-1;
-   while (low <= high)
-   {
-      mid = (low + high) / 2;
-      if ((cond = strcmp(symbol, keytab[mid].symbol)) < 0)
-        high = mid - 1;
-      else if (cond > 0)
-        low = mid + 1;
-      else
-        return keytab[mid].address;
-   }
-   return -1;
-}
-
-#define COMMENT "//"
-#define C_SEPERATOR ";"
-#define C_EQUALS "="
-
-struct node *talloc(void)
-{
-  return malloc(sizeof(struct node));
-}
-
-void insert(struct node *node, char *symbol, int address)
-{
-  int cond;
-
-  if (node == NULL)
-  {
-    node = malloc(sizeof(struct node));
-    *node->symbol = strdup(symbol);
-    *node->address = address;
-    *node->left = *node->right = NULL;
+    if (counter == chunk) {
+      chunk *= 2;
+      test = realloc(buffer, chunk);
+      if (!test) {
+        free(buffer);
+        exit(1);
+      } else {
+        buffer = test;
+      }
+    }
   }
-  else if ((cond = strcmp(symbol, node->symbol)) == 0)
-  {
-    printf("Element already inserted\n");
-    return; // @todo: throw an error?
-  } 
-  else if (cond < 0)
-  {
-    insert(node->left, symbol, address);
-  } 
-  else 
-  {
-    insert(node->right, symbol, address);
+
+  buffer[counter] = '\0';
+  if (realloc(buffer, counter + 1) == NULL) {
+    exit(1);
   }
+  return buffer;
 }
-void treeprint(struct node *node)
-{
-  if (node != NULL) {
-    treeprint(node->left);
-    printf("%s - %d\n", node->symbol, node->address);
-    treeprint(node->right);
+
+void runFile(char *filename) {
+  FILE *fp = NULL;
+  struct List symbol_table;
+  
+  if ((fp = fopen(filename, "r")) == NULL) {
+    fprintf(stderr, "Could not open file.\n");
+    exit(1);
   }
+
+  // populate symbol table with pre-defined symbols
+  populate_symbol_table(&symbol_table);
+
+  // First run: remove comments, white spaces and extra break lines, add labels to symbols table with 
+  // their corresponding line number;
+  char *first = first_pass(fp);
+  printf("%s\n", first); 
+  // parse file and write out binary output file
+  //char** second_pass = parse(first_pass, variables);
+  // free(cleaned_input)
+  // free(cleaned_input)
+  
+  freeList(&symbol_table);
+  fclose(fp);
 }
-struct node *populate_predefined_variables(struct node *variables) 
-{
-  insert(variables, "R0", 0);
-  insert(variables, "R1", 1);
-  insert(variables, "R2", 2);
-  insert(variables, "R3", 3);
-  insert(variables, "R4", 4);
-  insert(variables, "R5", 5);
-  insert(variables, "R6", 6);
-  insert(variables, "R7", 7);
-  insert(variables, "R8", 8);
-  insert(variables, "R9", 9);
-  insert(variables, "R10", 10);
-  insert(variables, "R11", 11);
-  insert(variables, "R12", 12);
-  insert(variables, "R13", 13);
-  insert(variables, "R14", 14);
-  insert(variables, "R15", 15);
-  insert(variables, "ARG", 2);
-  insert(variables, "KBD", 24576);
-  insert(variables, "LCL", 1);
-  insert(variables, "SCREEN", 16384);
-  insert(variables, "SP", 0);
-  insert(variables, "THAT", 4);
-  insert(variables, "THIS", 3);
-  return variables;
-}
+
+
 
 int main(int argc, char **argv) 
 {
-  struct node *variables = NULL;
-  // populate variables table
-  //populate_predefined_variables(variables);
-  insert(variables, "R0", 0);
-  // do a first pass to extract label declaration 
-  // extract_labels(variables);
-  
-  // parse file and write out binary output file
-  // parse(root);
-  
-  treeprint(variables);
+   if (argc != 2) {
+    fprintf(stderr, "Please provide a file.\n");
+    exit(1);
+  } else {
+    runFile(argv[1]);
+  }
   return 0;
 }
