71fc9fc80b383887bd833a06781bcde42527c915
Alejandro Gomez <agomez314@protonmail.com>
2022-03-17 07:46:20 -0600

    a instruction parsing

--- projects/assembler/parser.c
+++ projects/assembler/parser.c
@@ -4,6 +4,10 @@
 #include <string.h>
 #include "parser.h"
 
+bool isValidNumber(char *);
+char *convertToBin(int);
+char *pad16(int);
+
 bool parse(char *processed_file, struct keytab_list *variables) {
   int i = 0, j = 0;
   while (processed_file[i] != EOF) {
@@ -18,14 +22,36 @@ bool parse(char *processed_file, struct keytab_list *variables) {
 
       char *tmp = malloc((j-i+1)*sizeof(char));
       strlcpy(tmp, (processed_file+i), (j-i));
-      int value = atoi(tmp);
-      if (value != -1) {
+      if (isValidNumber(tmp)) {
         // convert to binary and print
+        int value = atoi(tmp);
+        printf("%d\n", pad16(convertToBin(value)));
       } else {
         // is not a number, add as variable to symbol table
         addVariable(variables, tmp);
       }
+      free(tmp);
     }
   }
   return true;
+}
+
+bool isValidNumber(char *str) {
+  int i = 0;
+  while (str[i] != EOF) {
+    if (str[i] < '0' || str[i] > '9') return false;
+    i++;
+  }
+  return true;
+}
+
+char *convertToBin(int num) {
+  char *result = malloc(sizeof(char)*16); // max allowed size is 2^16 or 65536
+  int i = 0;
+  while (num != 0) {
+    result[i] = num % 2;
+    num /= 2;
+    i++;
+  }
+  return result;
 }
